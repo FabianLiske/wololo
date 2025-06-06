1614d90183fb180b221b073d135b761a391297c1
Fabian Liske <19373177+FabianLiske@users.noreply.github.com>
2025-06-06 19:18:25 +0200

    Init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Wake on LAN (WoL) utility for RaspberryPi
+
+## Needed
+This tool and all scripts assume that you are running a regular RaspberyPi 3/4/5 Model B and have installed Ubuntu.
+
+## Installation
+Make setup script executable with
+```console
+chmod +x setup.sh
+```
+then run with
+```console
+./setup.sh
+```
+
+## Configuration
+
+## Usage
---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+set -euo pipefail
+
+# 1. Check for Python 3.11
+if ! command -v python3.11 > /dev/null 2>&1: then
+    echo "Error: Python 3.11 is not installed or not in PATH!"
+    echo "Please install Python 3.11, for example via 'sudo apt install python3.11 python3.11-venv') and try again."
+    exit 1
+fi
+
+# 2. Remove existing venv
+if [ -d "venv" ]; then
+    echo "Directory 'venv' already exists, removing ..."
+    rm -rf venv
+fi
+
+# 3. apt-packages
+sudo apt update
+sudo xargs -a apt-packages.txt apt install -y
+
+# 4. Create venv
+echo "Create venv with Python 3.11 ..."
+python3.11 -m venv --system-site-packages venv
+
+# 5. Activate and upgrade pip
+echo "Activate venv and upgrade pip ..."
+source venv/bin/activate
+pip install --upgrade pip
+
+# 6. Install Python packages
+echo "Installing Python packages ..."
+pip install -r python-packages.txt
+
+echo "Done. Python 3.11 venv set up."
