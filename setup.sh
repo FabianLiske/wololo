7e603201558d73dc9d01bc8a9fc7ac520270104c
Fabian Liske <19373177+FabianLiske@users.noreply.github.com>
2025-06-06 21:40:51 +0200

    Python bumped to 3.13

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 set -euo pipefail
 
-# 1. Check for Python 3.11
-if ! command -v python3.11 > /dev/null 2>&1; then
-    echo "Error: Python 3.11 is not installed or not in PATH!"
-    echo "Please install Python 3.11, for example via 'sudo apt install python3.11 python3.11-venv') and try again."
+# 1. Check for Python 3.13
+if ! command -v python3.13 > /dev/null 2>&1; then
+    echo "Error: Python 3.13 is not installed or not in PATH!"
+    echo "Please install Python 3.13, for example via 'sudo apt install python3.13 python3.13-venv') and try again."
     exit 1
 fi
 
@@ -19,8 +19,8 @@ sudo apt update
 sudo xargs -a apt-packages.txt apt install -y
 
 # 4. Create venv
-echo "Create venv with Python 3.11 ..."
-python3.11 -m venv --system-site-packages venv
+echo "Create venv with Python 3.13 ..."
+python3.13 -m venv --system-site-packages venv
 
 # 5. Activate and upgrade pip
 echo "Activate venv and upgrade pip ..."
@@ -31,4 +31,4 @@ pip install --upgrade pip setuptools wheel
 echo "Installing Python packages ..."
 pip install -r python-packages.txt
 
-echo "Done. Python 3.11 venv set up."
+echo "Done. Python 3.13 venv set up."
